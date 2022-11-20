eaf53b85953747df027fcf972a33e7adb783fabf
INFORMATIC0 <86555103+INFORMATIC0@users.noreply.github.com>
2022-11-20 05:40:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return 'Hola'
 @app.route('/getToken')
 def getTok():
 	getToken = requests.session()
