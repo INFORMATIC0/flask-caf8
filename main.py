f6cb30a71ee87f413186cc2c8abccbdebfc0af3d
INFORMATIC0 <86555103+INFORMATIC0@users.noreply.github.com>
2022-11-20 05:38:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template, send_file
 import os
-
+import requests
+from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route('/getToken')
+def getTok():
+	getToken = requests.session()
+	t = getToken.get('https://anuarioeco.uo.edu.cu/index.php/aeco/login',headers={"User-Agent":"TechDev_WebClient v01"})
+	token = BeautifulSoup(t.text,"html.parser").find('input',attrs={"name":"csrfToken"})["value"]
+	return token
 
 
 if __name__ == '__main__':
